69c5e36f95ea907652f980f90fe11445bfde8e6e
Cahyo Sasongko <yyos.dev@gmail.com>
2023-12-03 16:34:36 +0700

    added a constant size

---
+++ src/constant/size.ts
@@ -0,0 +1,9 @@
+import { Dimensions } from "react-native";
+
+export const INPUT_HEIGHT = 52;
+export const HEADER_HEIGHT = 60;
+export const BOTTOM_TAB_BAR_HEIGHT = 66;
+export const WINDOW_WIDTH = Dimensions.get("window").width;
+export const WINDOW_HEIGHT = Dimensions.get("window").height;
+export const SCREEN_WIDTH = Dimensions.get("screen").width;
+export const SCREEN_HEIGHT = Dimensions.get("screen").height;

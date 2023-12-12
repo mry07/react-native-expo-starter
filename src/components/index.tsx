795b5a3643ccef2d91ab5c346ae4a7333eb59c0c
Cahyo Sasongko <yyos.dev@gmail.com>
2023-12-12 23:18:35 +0700

    updated a component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
 export { default as Text } from "./common/text/Text";
-// export { default as Input } from "./common/input/Input";
-// export { default as Picker } from "./common/picker/Picker";
+export { default as Input } from "./common/input/Input";
+export { default as Picker } from "./common/picker/Picker";
 export { default as Button } from "./common/button/Button";

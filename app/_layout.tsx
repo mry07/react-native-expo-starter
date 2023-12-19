942727ae79bc2dda46bfec85ae35341eb7d61f4f
Cahyo Sasongko <yyos.dev@gmail.com>
2023-12-19 20:31:42 +0700

    setup the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,21 @@
-import React from "react";
 import fonts from "../src/constant/fonts";
+import React from "react";
+import Context from "../src/context";
+import Interceptors from "../src/service/api/Interceptors";
 import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { Slot } from "expo-router";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { Slot, SplashScreen } from "expo-router";
-import Interceptors from "../src/service/api/Interceptors";
 import { api } from "../src/service/api";
 import { LoadingModal } from "../src/components/modal";
 
 my = {};
 library.add(fab, far, fas);
 
+// Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
@@ -35,10 +38,12 @@ const Layout = () => {
   }
 
   return (
-    <Interceptors>
-      <Slot />
-      <LoadingModal visible={loading} />
-    </Interceptors>
+    <Context>
+      <Interceptors>
+        <Slot />
+        <LoadingModal visible={loading} />
+      </Interceptors>
+    </Context>
   );
 };
 

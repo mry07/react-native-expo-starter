5da7e163f1bfc5482633f286c96dc9f7c6a74141
Cahyo Sasongko <yyos.dev@gmail.com>
2023-12-23 23:03:54 +0700

    added a persistent login

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { AuthContext } from "../../src/context/auth";
 import { Redirect, Slot } from "expo-router";
+import { getStorage } from "../../src/utility/storage";
 
 const AppLayout = () => {
-  const { hasLogged } = React.useContext(AuthContext);
+  const { hasLogged, setHasLogged } = React.useContext(AuthContext);
+
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    (async () => {
+      const hasLogged = await getStorage("@has_logged");
+
+      setLoading(false);
+      setHasLogged(hasLogged === true);
+    })();
+  }, []);
+
+  if (loading) {
+    return null;
+  }
 
   if (!hasLogged) {
     return <Redirect href="/login" />;
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { router } from "expo-router";
 import { View, Text } from "react-native";
 import { AuthContext } from "../../src/context/auth";
+import { setStorage } from "../../src/utility/storage";
 
 const Login = () => {
   const { setHasLogged } = React.useContext(AuthContext);
 
   const onLogin = () => {
     setHasLogged(true);
+    setStorage("@has_logged", true);
     router.push("/");
   };
 

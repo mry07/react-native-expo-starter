a9abbc0ad9017b7f8c028d107f1665612a56e444
Cahyo Sasongko <yyos.dev@gmail.com>
2023-12-24 21:25:37 +0700

    fixes a importing switch component

--- src/components/common/switch/Switch.tsx
+++ src/components/common/switch/Switch.tsx
@@ -5,15 +5,15 @@ import Animated, {
   useAnimatedStyle,
   withTiming,
 } from "react-native-reanimated";
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 
-const SWITCH_WIDHT = 50;
-const SWITCH_HEIGHT = SWITCH_WIDHT / 1.7;
+const SWITCH_WIDTH = 50;
+const SWITCH_HEIGHT = SWITCH_WIDTH / 1.7;
 const SWITCH_PADDING = SWITCH_HEIGHT / 10;
 const SWITCH_RADIUS = SWITCH_HEIGHT / 2;
 const THUMB_SIZE = SWITCH_HEIGHT - SWITCH_PADDING * 2;
-const THUMB_TRANS_X = SWITCH_WIDHT - SWITCH_HEIGHT;
 const THUMB_RADIUS = THUMB_SIZE / 2;
+const THUMB_TRANS_X = SWITCH_WIDTH - SWITCH_HEIGHT;
 
 const Switch = () => {
   const [active, setActive] = React.useState(false);
@@ -31,30 +31,27 @@ const Switch = () => {
   }, [active]);
 
   return (
-    <Pressable
-      style={{
-        backgroundColor: Colors.black(0.9),
-        width: SWITCH_WIDHT,
-        height: SWITCH_HEIGHT,
-        borderRadius: SWITCH_RADIUS,
-        padding: SWITCH_PADDING,
-        overflow: "hidden",
-      }}
-      onPress={() => setActive(!active)}
-    >
-      <Animated.View
-        style={[
-          {
-            width: THUMB_SIZE,
-            height: THUMB_SIZE,
-            borderRadius: THUMB_RADIUS,
-            backgroundColor: Colors.white(),
-          },
-          thumbStyles,
-        ]}
-      />
+    <Pressable style={styles.container} onPress={() => setActive(!active)}>
+      <Animated.View style={[styles.thumb, thumbStyles]} />
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.black(0.8),
+    width: SWITCH_WIDTH,
+    height: SWITCH_HEIGHT,
+    borderRadius: SWITCH_RADIUS,
+    padding: SWITCH_PADDING,
+    overflow: "hidden",
+  },
+  thumb: {
+    width: THUMB_SIZE,
+    height: THUMB_SIZE,
+    borderRadius: THUMB_RADIUS,
+    backgroundColor: Colors.white(),
+  },
+});
 
 export default Switch;

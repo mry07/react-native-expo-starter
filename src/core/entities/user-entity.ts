8a5ec1d4b5bbf17b21cdfd3f131ca797ac85f655
Cahyo Sasongko <yyos.dev@gmail.com>
2024-01-09 23:01:29 +0700

    updated a user entity

--- src/core/entities/user-entity.ts
+++ src/core/entities/user-entity.ts
@@ -1,4 +1,4 @@
-export type UserEntity = {
+type UserEntity = {
   id: number;
   name: string;
   username: string;
@@ -22,64 +22,4 @@ export type UserEntity = {
   };
 };
 
-export type UserEntity2 = {
-  id: number;
-  firstName: string;
-  lastName: string;
-  maidenName: string;
-  age: number;
-  gender: string;
-  email: string;
-  phone: string;
-  username: string;
-  password: string;
-  birthDate: string;
-  image: string;
-  bloodGroup: string;
-  height: number;
-  weight: number;
-  eyeColor: string;
-  hair: {
-    color: string;
-    type: string;
-  };
-  domain: string;
-  ip: string;
-  address: {
-    address: string;
-    city: string;
-    coordinates: {
-      lat: number;
-      lng: number;
-    };
-    postalCode: string;
-    state: string;
-  };
-  macAddress: string;
-  university: string;
-  bank: {
-    cardExpire: string;
-    cardNumber: string;
-    cardType: string;
-    currency: string;
-    iban: string;
-  };
-  company: {
-    address: {
-      address: string;
-      city: string;
-      coordinates: {
-        lat: number;
-        lng: number;
-      };
-      postalCode: string;
-      state: string;
-    };
-    department: string;
-    name: string;
-    title: string;
-  };
-  ein: string;
-  ssn: string;
-  userAgent: string;
-};
+export default UserEntity;
